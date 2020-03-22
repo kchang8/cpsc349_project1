fc9588dee65e44d2d3d916e2c8cf96164de646cf
santransisco <santransisco@yahoo.com>
2020-03-21 22:54:34 -0700

    Now the init tables works

--- inittables.php
+++ inittables.php
@@ -1,13 +1,9 @@
 <?php
     include 'scripts/petscripts.php';
     //Replace these values for whatever your login info/database info is
-    $dbhost = 'localhost';
-    $dbuser = 'San';
-    $dbpassword = '1234';
-    $petDatabase = 'petdatabase';
 
-    $db = new PetDatabase('localhost','San','1234','petdatabasetest');
+    $db = new PetDatabase('localhost','San','1234','petdatabase2');
     
-    $db->create_tables();
+    $db->init_tables();
 
 ?>
--- scripts/petscripts.php
+++ scripts/petscripts.php
@@ -26,19 +26,24 @@
 
         //THIS IS A DEBUG FUNCTION, NOT INTENDED FOR PRODUCTION
         function init_tables(){
-            $query = "DROP TABLE IF EXISTS 'owners'; CREATE TABLE `owners` ( `ID` INT NOT NULL AUTO_INCREMENT , `fname` VARCHAR(64) NOT NULL ";
+            $query = "DROP TABLE IF EXISTS `$this->petDatabase`.`owners`; CREATE TABLE `$this->petDatabase`.`owners` ( `ID` INT NOT NULL AUTO_INCREMENT , `fname` VARCHAR(64) NOT NULL ";
             $query.= ", `lname` VARCHAR(64) NOT NULL , `city` VARCHAR(64) NOT NULL , `age` INT NOT NULL , `email` VARCHAR(254) ";
             $query.= "NOT NULL , `password` VARCHAR(254) NOT NULL , `gender` VARCHAR(4) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;";
             echo $query;
             $this->conn->query($query);
-            $query =    "DROP TABLE IF EXISTS 'pets'; CREATE TABLE `pets` ( `ID` int(11) NOT NULL, `OwnerID` int(11) NOT NULL, `Name` ";
+            
+            $query =    "CREATE TABLE `$this->petDatabase`.`pets` ( `ID` INT NOT NULL, `OwnerID` INT NOT NULL, `Name` ";
             $query .=   "varchar(64) NOT NULL, `Species` varchar(64) NOT NULL, `Breed` varchar(64) NOT NULL, `Age` int(11) NOT NULL, ";
-            $query .=   "`Bio` varchar(512) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+            $query .=   "`Bio` varchar(512) NOT NULL, PRIMARY KEY (`ID`)) ENGINE=InnoDB;";
+
             $this->conn->query($query);
-            $query = "DROP TABLE IF EXISTS 'playdates'; CREATE TABLE `playdates` ( `PetID_creator` int(11) NOT NULL, `OwnerID_creator` ";
+
+            $query = "CREATE TABLE `$this->petDatabase`.`playdates` ( `PetID_creator` int(11) NOT NULL, `OwnerID_creator` ";
             $query.= "int(11) NOT NULL, `PetID_responder` int(11) NOT NULL, `OwnerID_responder` int(11) NOT NULL, `Time` date NOT NULL, ";
             $query.= "`State` varchar(4) NOT NULL, `City` int(64) NOT NULL, `Confirmed` tinyint(1) NOT NULL DEFAULT 0 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
             $this->conn->query($query);
+            
+            
         }
 
         //THIS IS A DEBUG FUNCTION, NOT INTENDED FOR PRODUCTION
