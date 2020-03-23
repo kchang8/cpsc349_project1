9ac45f4a66d46a92c78d2aed992be1ea556e9df1
kchang8 <kailiec@csu.fullerton.edu>
2020-03-23 14:20:06 -0700

    added some stuff to files

--- profile.php
+++ profile.php
@@ -162,6 +162,10 @@
             </div>
         </section>
 
+        <footer class="page-footer font-small pt-3">
+            <div class="footer-copyright text-center py-3">© 2020 Copyright: Paw Me!</div>
+        </footer>
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js" charset="utf-8"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
