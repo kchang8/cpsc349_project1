500677469cedcfa436f5d8405cf99ac9f88994a3
santransisco <santransisco@yahoo.com>
2020-03-23 17:22:18 -0700

    Working on profile editing

--- profile.php
+++ profile.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <title>Profile</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu:500&display=swap">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href= "stylesheets/styles.css">
@@ -34,6 +36,7 @@
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Profile</a>
                         <div class="dropdown-menu">
                             <a href="#" class="dropdown-item">Settings</a>
+                            <div class="dropdown-divider"></div>
                             <a href="#" class="dropdown-item">Log Out</a>
                         </div>
                     </li>
@@ -104,7 +107,7 @@
             </div>
         </section>
 
-        <hr>
+      
         <!--pet form info-->
 
         <section class="container">
@@ -113,7 +116,7 @@
             </header>
             <div class="panel panel-default">
                 <div class="panel-body">
-                <form data-pet-signup="form" method="POST" action="" enctype="multipart/form-data">
+                <form id = "pet-info" data-pet-signup="form" method="POST" action="scripts/update_pet.php" enctype="multipart/form-data">
                         <div class="box text-center">
                             <img src="imgs/placeholderImage.jpg" class="rounded-circle" alt="placeholder image">
                         </div>
@@ -121,40 +124,40 @@
                         <div class="form-group row pt-3 form-font">
                             <div class="col-md-7">
                                 <label for="pNameInput">Pet's name:</label>
-                                <input class="form-control" name="petName" id="pNameInput" readonly>
+                                <input class="form-control" name="petName" id="pNameInput" readonly="readonly">
                             </div>
 
                             <div class="col-md-3">
                                 <label for="genderInput">Gender:</label>
-                                <input class="form-control" id="genderInput" name="gender" readonly>
+                                <input class="form-control" id="genderInput" name="gender" readonly="readonly">
                             </div>
 
                             <div class="col-md-2">
                                 <label for="pAgeInput">Age:</label>
-                                <input class="form-control" name="petAge" id="pAgeInput" readonly>
+                                <input class="form-control" name="petAge" id="pAgeInput" readonly="readonly">
                             </div>
                         </div>
 
                         <div class="form-group row pt-3 form-font">
                             <div class="col-md-6">
                                 <label for="animalInput">What animal is your pet?</label>
-                                <input class="form-control" name="animal" id="animalInput" readonly>
+                                <input class="form-control" name="animal" id="animalInput" readonly="readonly">
                             </div>
 
                             <div class="col-md-6">
                                 <label for="breedInput">What breed is your pet?</label>
-                            <input class="form-control" name="breed" id="breedInput" readonly>
+                                <input class="form-control" name="breed" id="breedInput" readonly="readonly">
                             </div>
                         </div>
 
                         <div class="form-group pt-3 form-font">
                             <label for="aboutPetInput">About me, the pet:</label>
-                            <textarea class="form-control" name="aboutPet" id="aboutPetInput" rows="3" readonly></textarea>
+                            <textarea class="form-control" name="aboutPet" id="aboutPetInput" rows="3" readonly="true">yoyoy</textarea>
                         </div>
                         
                         <div class="row justify-content-around">
-                            <button type="edit" class="btn btn-default">Edit Profile</button>
-                            <button type="delete" class="btn btn-default">Delete Profile</button>
+                            <button id ="edit-button" type="edit" class="btn btn-default">Edit Profile</button>
+                            <button id ="delete-button" type="delete" class="btn btn-default">Delete Profile</button>
                         </div>
                         
                     </form>
@@ -165,7 +168,7 @@
         <footer class="page-footer font-small pt-3">
             <div class="footer-copyright text-center py-3">© 2020 Copyright: Paw Me!</div>
         </footer>
-
+        <script src="scripts/editprofile.js" charset="utf-8"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js" charset="utf-8"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
---
+++ scripts/editprofile.js
@@ -0,0 +1,28 @@
+(function(window){
+    'use strict';
+    var $ = window.jQuery;
+    var edit_button = $("#edit-button");
+    var delete_button = $("#delete-button"):
+    var toggle = 0;
+
+    edit_button.on('click', function(event){
+        event.preventDefault();
+        if(toggle ===0){
+            console.log("We prevented the default edit event");
+            $(".form-control").attr("readonly", false);
+            edit_button.text("Save Changes");
+            toggle =1;
+        }
+        else if(toggle ===1){
+            document.getElementById("pet-info").submit();
+        }
+    });
+
+    delete_button.on('click',function(event){
+        event.preventDefault();
+    });
+
+
+
+
+})(window);
