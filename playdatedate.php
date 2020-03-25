94c7c02bc1b59f9b6e648d67f50f5abd98668b2c
natertater23 <angelbaseball2@gmail.com>
2020-03-24 22:18:49 -0700

    Update playdatedate.php

--- playdatedate.php
+++ playdatedate.php
@@ -10,14 +10,12 @@
   $conn = mysqli_connect($dbHost,$dbUsername,$dbPass,$dbName);
   $ID = $_SESSION["ID"];
   $query = "SELECT * FROM `playdates` WHERE OwnerID = $ID";    
-  $result = mysqli_result($conn,$query); 
-  //$x = mysqli_num_rows($result);
-  //$log = "<script> console.log('$x')</script>";
- // echo $log;
-  if(mysqli_num_rows($result)!=0){
-      $_SESSION["err"] = 1;
-      header ("Location: ../profile.php");
-        }
+  $result = mysqli_query($conn,$query); 
+  if($result == TRUE){
+    $_SESSION["err"] = 1;
+    header ("Location: ../ownerSignup.php");
+  }
+ 
 ?>
 
 <!DOCTYPE html>
