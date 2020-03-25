bb0508057011a3e740ec9f6db832942c4a03de78
santransisco <santransisco@yahoo.com>
2020-03-24 21:18:15 -0700

    Update redirect_to_login.php

--- scripts/redirect_to_login.php
+++ scripts/redirect_to_login.php
@@ -1,6 +1,6 @@
 <?php
 function redirect_to_login(){
-    if(isset($_SESSION["ID"])&&isset($_COOKIE["pet-owner"])){
+    if(isset($_SESSION["ID"])||isset($_COOKIE["pet-owner"])){
         echo  "<script> console.log(\"The session is set\") </script>";
     }
     else{
